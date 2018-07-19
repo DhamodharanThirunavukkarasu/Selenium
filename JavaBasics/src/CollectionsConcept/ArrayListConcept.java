5e0a09b675a69344ae41017d2c76323d2a96a88c
Dhamodharan <Dhamodharan@Dhamodharan-PC>
2018-07-19 09:00:31 +0530

    Added Linked list class

--- JavaBasics/src/CollectionsConcept/ArrayListConcept.java
+++ JavaBasics/src/CollectionsConcept/ArrayListConcept.java
@@ -1,6 +1,7 @@
 package CollectionsConcept;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ArrayListConcept {
 
@@ -15,6 +16,24 @@ public class ArrayListConcept {
 		for(int i=0;i<ar.size();i++)
 		{
 			System.out.println(ar.get(i));
+		}
+		
+		
+		Employee emp1=new Employee(10,"dhamo", "ece");
+		Employee emp2=new Employee(20,"magesh", "IT");
+		Employee emp3=new Employee(30,"aadhi", "CSE");
+		
+		ArrayList<Employee> ar1=new ArrayList<Employee>();
+		ar1.add(emp1);
+		ar1.add(emp2);
+		ar1.add(emp3);
+		Iterator<Employee>it=ar1.iterator();
+		while (it.hasNext()) {
+			Employee emp =it.next();
+			System.out.print(emp.name);
+			System.out.println(emp.a);
+			System.out.println(emp.dep);
+			
 		}
 
 	}
---
+++ JavaBasics/src/CollectionsConcept/LinkedListConcept.java
@@ -0,0 +1,78 @@
+package CollectionsConcept;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import org.apache.log4j.varia.LevelMatchFilter;
+
+public class LinkedListConcept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LinkedList<String> l=new LinkedList<String>();
+		//add
+		l.add("Dhamo");
+		l.add("Dharan");
+		l.add("Susi");
+		System.out.println("Content of linkedlist"+l);
+		//addfirst and last index
+		
+		l.addFirst("Amma");
+		l.addLast("Appa");
+		System.out.println("Content of linkedlist"+l);
+		//add particular index
+		l.add(1, "Saranya");
+		l.add(5, "Prathap");
+		l.add(6,"Prakash");
+		System.out.println("Content of linkedlist"+l);
+		
+		//Remove
+		
+	l.removeFirst();
+	System.out.println("Content of linkedlist"+l);
+	
+	//for loop
+	System.out.println("****For loop iteration*****");
+	
+	for(int i=0;i<l.size();i++)
+	{
+	
+		System.out.println(l.get(i));
+			}
+	//Advance for loop
+	System.out.println("****For loop iteration*****");
+		for(String str:l)
+		{
+			System.out.println(str);
+		}
+	
+		//Iterator loop
+		System.out.println("****Using iterator iteration*****");
+		
+		Iterator<String>it=l.iterator();
+		while(it.hasNext())
+		{
+			System.out.println(it.next());
+		}
+		
+		System.out.println("****Using while loop iteration*****");
+		
+		int num=0;
+		while(l.size()>num)
+		{
+			System.out.println(l.get(num));
+			num++;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
